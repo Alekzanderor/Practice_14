6198dda069fdf052856226e570ec4a6ba0dada36
Alekzander <alekzanderor@gmail.com>
2020-03-28 19:34:54 +0400

    Initial commit

---
+++ src/Practice_14.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Practice_14 {
+
+    public static void main(String[] args)
+    {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input number: ");
+        String s=sc.nextLine();
+
+        System.out.println("s: ");
+        System.out.println(s);
+
+        int x=Integer.parseInt(s);
+        System.out.println("x: ");
+        System.out.println(x);
+
+        double y=(double)x;
+        System.out.println("y: ");
+        System.out.println(y);
+
+    }
+
+}
+
